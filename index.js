b6bf7c51ac3ba1a55a10554aa4f6a57dc74d34ba
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-06-30 14:03:25 +0500

    add

---
+++ index.js
@@ -0,0 +1,12 @@
+// var fruits = ["apple", "banana", "orange", "pineapple", "grapes"]
+// for(var fruitIndex = 0; fruitIndex < 5; fruitIndex++){
+//     console.log(fruits[fruitIndex])
+//     }
+// var animals = ["cat", "dog", "cow", "monkey", "lion"]
+// for(var animalsIndex = 0; )
+
+    var studentNames = []
+    for(var studentNames = 0; studentNames < 5; studentNames++){
+      var studentNames =  prompt("plx enter your name: ")
+      }
+    console.log(studentNames)
